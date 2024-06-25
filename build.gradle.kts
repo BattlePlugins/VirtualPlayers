b4e853890ff40d303b8f59ca9769f19d05c90497
RednedEpic <redned235@gmail.com>
2024-06-24 20:40:52 -0500

    Deploy to maven repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("java-library")
+    id("maven-publish")
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
@@ -19,6 +20,53 @@ allprojects {
     apply {
         plugin("java")
         plugin("java-library")
+        plugin("maven-publish")
         plugin("com.github.johnrengelman.shadow")
+    }
+
+    publishing {
+        val isSnapshot = "SNAPSHOT" in version.toString()
+
+        repositories {
+            maven {
+                name = "battleplugins"
+                url = uri("https://repo.battleplugins.org/${if (isSnapshot) "snapshots" else "releases"}")
+                credentials(PasswordCredentials::class)
+                authentication {
+                    create("basic")
+                }
+            }
+
+            publications {
+                create<MavenPublication>("mavenJava") {
+                    from(components["java"])
+                    pom {
+                        packaging = "jar"
+                        url.set("https://github.com/BattlePlugins/VirtualPlayers")
+
+                        scm {
+                            connection.set("scm:git:git://github.com/BattlePlugins/VirtualPlayers.git")
+                            developerConnection.set("scm:git:ssh://github.com/BattlePlugins/VirtualPlayers.git")
+                            url.set("https://github.com/BattlePlugins/VirtualPlayers");
+                        }
+
+                        licenses {
+                            license {
+                                name.set("GNU General Public License v3.0")
+                                url.set("https://www.gnu.org/licenses/gpl-3.0.html")
+                            }
+                        }
+
+                        developers {
+                            developer {
+                                name.set("BattlePlugins Team")
+                                organization.set("BattlePlugins")
+                                organizationUrl.set("https://github.com/BattlePlugins")
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
